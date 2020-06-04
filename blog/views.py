7fedc51cbe1ba106e60aaae0c9c8c1f06d97ccef
elcooper44 <elcooper0144@gmail.com>
2020-06-04 12:34:13 +0100

    Added CV

--- blog/views.py
+++ blog/views.py
@@ -39,3 +39,8 @@ def post_edit(request, pk):
     else:
         form = PostForm(instance=post)
     return render(request, 'blog/post_edit.html', {'form': form})
+
+def cv_pdf(request):
+    return render(request, 'blog/cv_pdf.html', {})
+   
+    
