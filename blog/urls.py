fd3c85d6c03b78edf5013f2d9e8e9c10b3336eea
elcooper44 <elcooper0144@gmail.com>
2020-06-08 09:52:03 +0100

    Debug

--- blog/urls.py
+++ blog/urls.py
@@ -6,5 +6,5 @@ urlpatterns = [
     path('post/<int:pk>/', views.post_detail, name='post_detail'),
     path('post/new/', views.post_new, name='post_new'),
     path('post/<int:pk>/edit/', views.post_edit, name='post_edit'),
-    path('cv', views.cv_pdf, name='cd_pdf'),
+    path('cv', views.cv_pdf, name='cv_pdf'),
 ]
